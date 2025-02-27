8b00a57ffafffc8c180e5790db27b17fbbc9c29a
Abdelghani Benbyi <76821097+abdoubenbyi@users.noreply.github.com>
2025-02-27 16:59:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,25 +6,19 @@ url: str = os.environ.get('URL')
 key: str = os.environ.get('KEY')
 supabase: Client = create_client(url, key)
 
-response = supabase.table('ur_cristiano').delete().neq("id", -1).execute()
+projects = {  "ur_cristiano": "UCtxD0x6AuNNqdXO9Wp5GHew",
+  "aboflah": "UCqq5n-Oe-r1EEHI3yvhVJcA"}
 
-videos = scrapetube.get_channel("UCtxD0x6AuNNqdXO9Wp5GHew",sort_by="oldest")
-
-i = 1
-for video in videos:
-    #print(video['videoId'])
-    response = (
-    supabase.table("ur_cristiano")
-    .insert({"id": i ,"title": video['title']['runs'][0]['text'], "link": f'http://www.youtube.com/watch?v={video["videoId"]}'})
-    .execute()
-    )
-    i = i + 1
-
+for project in projects.keys():
+    response = supabase.table(project).delete().neq("id", -1).execute()
+    videos = scrapetube.get_channel(projects[project],sort_by="oldest")
+    i = 1
+    for video in videos:
+        #print(video['videoId'])
+        response = (
+        supabase.table(project)
+        .insert({"id": i ,"title": video['title']['runs'][0]['text'], "link": f'http://www.youtube.com/watch?v={video["videoId"]}'})
+        .execute()
+        )
+        i = i + 1
 print(f"Done")
-
-
-"""response = (
-    supabase.table("test")
-    .insert({})
-    .execute())
-print('Inserted succefully', )"""
