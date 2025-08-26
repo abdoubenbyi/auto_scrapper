3eeb1b6e69e80822e21bd9a011c5fcd82215d054
Abdelghani Benbyi <76821097+abdoubenbyi@users.noreply.github.com>
2025-08-26 16:17:49 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
 import os
 from supabase import create_client, Client
 
-url: str = os.environ.get('URL')
-key: str = os.environ.get('KEY')
+url: str = os.environ.get('URL2')
+key: str = os.environ.get('KEY2')
+
+supabase: Client = create_client(url, key)
+response = supabase.table("table").insert({}).execute()
