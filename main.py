4f1821d3ea40a9d36127e32705b6c7bf9bb936b5
Abdelghani Benbyi <76821097+abdoubenbyi@users.noreply.github.com>
2025-09-03 12:24:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ db1: str = os.environ.get('db1')
 db2: str = os.environ.get('db2')
 supabase: Client = create_client(url, key)
 
-projects = {  db1: "UCtxD0x6AuNNqdXO9Wp5GHew",
-  db2: "UCqq5n-Oe-r1EEHI3yvhVJcA"}
+projects = {  "ur_cristiano": "UCtxD0x6AuNNqdXO9Wp5GHew",
+  "aboflah": "UCqq5n-Oe-r1EEHI3yvhVJcA"}
 
 for project in projects.keys():
     response = supabase.table(project).delete().neq("id", -1).execute()
     videos = scrapetube.get_channel(projects[project],sort_by="newest")
     i = 1
     for video in videos:
-          #print(video['videoId'])
-          response = (
-          supabase.table(project)
-          .insert({"id": i ,"title": video['title']['runs'][0]['text'], "link": f'http://www.youtube.com/watch?v={video["videoId"]}'})
-          .execute()
-          )
-          i = i + 1
+        #print(video['videoId'])
+        response = (
+        supabase.table(project)
+        .insert({"id": i ,"title": video['title']['runs'][0]['text'], "link": f'http://www.youtube.com/watch?v={video["videoId"]}'})
+        .execute()
+        )
+        i = i + 1
 print(f"Done")
